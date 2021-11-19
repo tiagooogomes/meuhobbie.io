727ebb2d885e37225ac9fa07ad9ea3b2e228d31a
tiagooogomes <91339622+tiagooogomes@users.noreply.github.com>
2021-11-19 09:22:58 -0300

    Primeira versão

---
+++ js.js
@@ -0,0 +1,63 @@
+let h1 = document.getElementById("origami");
+h1.innerHTML = "ORIGAMI";
+
+let h2 = document.getElementById("sub");
+h2.innerHTML = "A arte japonesa de dobrar papel";
+
+document.getElementById("photo").src = "img/eu.jpg";
+
+let p = document.getElementById("description");
+p.innerHTML = "Olá! Meu nome é Tiago Santos de Jesus, tenho 20 anos e sou da Bahia. Desde pequeno, quando eu tive o primeiro contato com o origami, eu me apeguei a essa arte de dobrar papal. No inicio foi complicado aprender as dobras, mas não demorou muito até eu dominar as tecnicas e fazer diversos modelos considerados complicados. Desde aquela epoca eu faço origamis pois além de ser um passatempo que eu gosto muito, ele ainda trás beneficios a saúde como ajuda na concetração, desenvolve a mamória e pasciência, ajuda na coordenação motora e outros muitos outros beneficios.";
+
+document.getElementById("tisuru").src = "img/tisuru.jpg";
+
+let about = document.getElementById("about");
+about.innerHTML = "Origami é uma técnica japonesa, uma arte de dobrar papel, e existe há mais de um século, fazendo jus ao significado do termo, que é fazer dobras de papel, sem cortes e nem colas, para criar objetos e outros seres.";
+
+let hobby = document.getElementById("hobby");
+hobby.innerHTML = "MEU HOBBY";
+
+let slide = document.getElementById("slide");
+slide.innerHTML = "EXEMPLOS BONITOS";
+
+document.getElementById("3").src = "img/3.jpg";
+document.getElementById("3d").src = "img/3d.jpg";
+document.getElementById("alien").src = "img/alien.jpg";
+document.getElementById("anjo").src = "img/anjo.jpg";
+document.getElementById("galo").src = "img/galo.jpg";
+document.getElementById("coisa").src = "img/coisa.jpg";
+document.getElementById("dragao").src = "img/dragao.jpg";
+document.getElementById("et").src = "img/et.jpg";
+document.getElementById("gaca").src = "img/gaca.jpg";
+document.getElementById("girafa").src = "img/girafa.jpg";
+document.getElementById("pessoa").src = "img/pessoa.jpg";
+
+let refe = document.getElementById("refe");
+refe.innerHTML = "YOUTUBERS";
+
+document.getElementById("tuber1").src = "img/jo.jpg";
+document.getElementById("tuber2").src = "img/easy.jpg";
+document.getElementById("tuber3").src = "img/mariano.jpg";
+
+let span = document.getElementById("span1");
+span.innerHTML = "Jo Nakashima";
+
+let span2 = document.getElementById("span2");
+span2.innerHTML = "Easy Origami";
+
+let span3 = document.getElementById("span3");
+span3.innerHTML = "MarianoZavala";
+
+document.getElementById("link1").href = "https://youtu.be/VMnaXwivfDs";
+document.getElementById("link1").target = "_blank";
+
+document.getElementById("link2").href = "https://youtu.be/pzS0ToWZ9DA";
+document.getElementById("link2").target = "_blank";
+
+document.getElementById("link3").href = "https://youtu.be/vdo_Lyk7X3c";
+document.getElementById("link3").target = "_blank";
+
+
+
+
+
